f833fb4dec2b9dadc6023412e168b34dc5f7b336
start.vaadin.com <no-reply@vaadin.com>
2022-04-12 08:54:08 +0000

    Generated project

---
+++ src/main/java/com/ronnelle/application/views/home/HomeView.java
@@ -0,0 +1,30 @@
+package com.ronnelle.application.views.home;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@PageTitle("Home")
+@Route(value = "")
+public class HomeView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public HomeView() {
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+        add(name, sayHello);
+    }
+
+}
